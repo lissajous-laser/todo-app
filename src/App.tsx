a6790715cab33f521f994d7ea836edbe1cd468e5
Quang Tran <quangtran@quang-a520i.net>
2022-10-27 12:03:49 +1100

    Fixed typo causing misaligned check mark

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,6 @@ type State = {
   darkMode: boolean,
   listDisplayMode: ListDisplayMode,
   newTask: NewTask,
-  isMobileSize: boolean
   windowWidth: number
 }
 
@@ -402,7 +401,6 @@ function NewTaskEditor() {
         id: maxId + 1,
         ...newTask
       }
-
       dispatch(addTodo(taskToAdd));
     } 
     
@@ -443,7 +441,7 @@ function NewTaskEditor() {
     >
       <div {...buttonMaskStyle(newTask)}></div>
       {newTask.isComplete && <img
-        className="absolute left-2/4top-2/4-translate-x-2/4 -translate-y-2/4"
+        className="absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4"
         src={checkIcon}
         alt="check mark"
       />}
